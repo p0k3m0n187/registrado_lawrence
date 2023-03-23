36afeaea908cc2fa9fcc01001a8d727255a271d9
Grace An Velayo <graceann151@gmail.com>
2023-03-23 20:40:38 +0800

    SysArch Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <center>
+            <div style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL + 'https://img.freepik.com/free-vector/green-curve-abstract-background_53876-99569.jpg'})`
+            }}>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>Welcome to Agri-Shop!</h1>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2>Get all of your important fresh harvested agricultural products, fruits vegetables and seeds</h2>
+            <p>Our major goal is to five all of the items in one location so that farmers and buyers can</p>
+            <p>easily purchase supplies, seeds and many more which they can easily access.</p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+             <button type="button">Sign In!</button>
+            </div>
+        </center>
+    );
+}
+
+export default App;
